09713262ce32f2700a4103230d1221438a7e5267
Wilian-Hoffmann <wilian.hoffmann@unidavi.edu.br>
2024-05-07 20:28:37 -0300

    em andamento

---
+++ login.sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 08/05/2024 às 01:28
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `login`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `demandas`
+--
+
+CREATE TABLE `demandas` (
+  `id` int(11) NOT NULL,
+  `produto` varchar(255) NOT NULL,
+  `usuario` varchar(255) NOT NULL,
+  `data_demanda` date NOT NULL,
+  `meta` int(100) NOT NULL,
+  `producao` int(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `demandas`
+--
+
+INSERT INTO `demandas` (`id`, `produto`, `usuario`, `data_demanda`, `meta`, `producao`) VALUES
+(1, 'Shorts', 'Wilian', '2024-04-21', 20, 15),
+(6, 'Camisetas', 'Wendel', '2024-05-06', 50, NULL),
+(7, 'Cueca', 'Emanuel', '2024-05-08', 20, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(140) NOT NULL,
+  `email` varchar(140) NOT NULL,
+  `senha` varchar(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`) VALUES
+(1, 'Wilian', 'teste@teste.com', 'teste');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `demandas`
+--
+ALTER TABLE `demandas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `demandas`
+--
+ALTER TABLE `demandas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
